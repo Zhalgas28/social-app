eae94c460276eb3ba5a90ebab0c7e893bddc6cfb
Zhalgas Kabdulov <kabdulov1728@gmail.com>
2023-01-20 01:35:41 +0600

    hook useEffect for status

--- src/components/MyProfile/Info/Info.js
+++ src/components/MyProfile/Info/Info.js
@@ -1,65 +1,47 @@
-import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import styles from "./Info.module.css";
 
 const DEFAULT_AVATAR = "https://static.vecteezy.com/system/resources/previews/004/696/485/original/shadow-samurai-warrior-on-sunlight-vector.jpg"
 
-class Info extends React.Component {
-  state = {
-    editMode: false,
-		status: this.props.status
-  };
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevProps.status !== this.props.status) {
-			this.setState({
-				status: this.props.state
-			})
-		}
-	}
+function Info(props) {
+    const [status, setStatus] = useState(props.status)
+    const [editMode, setEditMode] = useState(false)
 
-  toggleEditMode = (bool) => {
-    this.setState({
-      editMode: bool,
-    });
-  }
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
-	onChangeStatus = (e) => {
-		this.setState({
-			status: e.target.value
-		})
-	}
-
-  render() {
     return (
-      <div className={styles.info}>
-        <div className={styles.ava}>
-          <img
-            src={this.props.profile.photos.large || DEFAULT_AVATAR}
-            alt="ava"
-          ></img>
-          {!this.state.editMode ? (
-            <div onDoubleClick={() => this.toggleEditMode(true)}>
-              {this.props.status || "-------"}
+        <div className={styles.info}>
+            <div className={styles.ava}>
+                <img
+                    src={props.profile.photos.large || DEFAULT_AVATAR}
+                    alt="ava"
+                ></img>
+                {!editMode ? (
+                    <div onDoubleClick={() => setEditMode(true)}>
+                        {status || "-------"}
+                    </div>
+                ) : (
+                    <input
+                        autoFocus={true}
+                        value={status}
+                        onBlur={() => {
+                            props.updateStatus(status)
+                            setEditMode(false);
+                        }}
+                        onChange={(event) => {setStatus(event.target.value)}}
+                    />
+                )}
             </div>
-          ) : (
-            <input
-              autoFocus={true}
-              value={this.state.status}
-              onBlur={() => {
-								this.props.updateStatus(this.state.status)
-                this.toggleEditMode(false);
-              }}
-							onChange={this.onChangeStatus}
-            />
-          )}
+            <div className={styles.description}>
+                <h2>{props.profile.fullName}</h2>
+                <p>{props.profile.lookingForAJobDescription}</p>
+            </div>
         </div>
-        <div className={styles.description}>
-          <h2>{this.props.profile.fullName}</h2>
-          <p>{this.props.profile.lookingForAJobDescription}</p>
-        </div>
-      </div>
     );
-  }
 }
 
 export default Info;
